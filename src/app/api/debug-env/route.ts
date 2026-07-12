9518cdf49af5cec006e886128f644759287cf44e
Roshini R <roshiniravi05@gmail.com>
2026-07-12 13:47:11 +0530

    chore: add TEST_VARIABLE check to debug-env route

--- src/app/api/debug-env/route.ts
+++ src/app/api/debug-env/route.ts
@@ -1,13 +1,11 @@
-import { NextResponse } from "next/server";
-
 export const dynamic = "force-dynamic";
 
 export async function GET() {
-  return NextResponse.json({
-    GOOGLE_CLIENT_ID_EXISTS: !!process.env.GOOGLE_CLIENT_ID,
-    GOOGLE_CLIENT_SECRET_EXISTS: !!process.env.GOOGLE_CLIENT_SECRET,
-    NEXTAUTH_SECRET_EXISTS: !!process.env.NEXTAUTH_SECRET,
-    NEXTAUTH_URL_EXISTS: !!process.env.NEXTAUTH_URL,
-    NEXTAUTH_URL_VALUE: process.env.NEXTAUTH_URL || "NOT_SET",
+  return Response.json({
+    TEST_VARIABLE: process.env.TEST_VARIABLE || false,
+    GOOGLE_CLIENT_ID: !!process.env.GOOGLE_CLIENT_ID,
+    GOOGLE_CLIENT_SECRET: !!process.env.GOOGLE_CLIENT_SECRET,
+    NEXTAUTH_SECRET: !!process.env.NEXTAUTH_SECRET,
+    NEXTAUTH_URL: !!process.env.NEXTAUTH_URL,
   });
 }
